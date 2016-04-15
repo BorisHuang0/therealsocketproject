9d3db2b44529773351742dfb5ee2d0b31aec6cf4
Abraham McIlvaine <abemac@udel.edu>
2016-04-14 22:19:26 -0400

    asdf

--- project/src/server.java
+++ project/src/server.java
@@ -10,21 +10,24 @@ public class server{
 		server.run();
 
 	}
-	
+
 	public void run() throws Exception{
-		ServerSocket ssocket = new ServerSocket(444);
+		ServerSocket ssocket = new ServerSocket(1025);
 		Socket SOCK = ssocket.accept();
 		InputStreamReader IR = new InputStreamReader(SOCK.getInputStream());
 		BufferedReader BR = new BufferedReader(IR);
-		
+
 		String MESSAGE = BR.readLine();
 		System.out.println(MESSAGE);
-		
+
 		if (MESSAGE != null) {
 			PrintStream PS = new PrintStream(SOCK.getOutputStream());
 			PS.println("MESSAGE received!");
 		}
-	
+		
+		ssocket.close();
+
+
 	}
 
 }
